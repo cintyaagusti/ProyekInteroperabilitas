b1813a96a1d63b2076ef8924f380254cc39d680a
Cintya Agusti Anindita <cintyaagusti99@mail.ugm.ac.id>
2020-03-30 07:44:26 +0700

    Updates

---
+++ precache-manifest.19268b4b3fa69b6091ca65e6633a0494.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b368a533809bdc80a8af0afc8cb0a81e",
+    "url": "/ProyekInteroperabilitas/index.html"
+  },
+  {
+    "revision": "e5d09c4ff597b432649a",
+    "url": "/ProyekInteroperabilitas/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "ff3ae1e56fccff2e2b0a",
+    "url": "/ProyekInteroperabilitas/static/js/2.f8e94f42.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ProyekInteroperabilitas/static/js/2.f8e94f42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5d09c4ff597b432649a",
+    "url": "/ProyekInteroperabilitas/static/js/main.852172b7.chunk.js"
+  },
+  {
+    "revision": "2dd0842fc582fc3ed9ad",
+    "url": "/ProyekInteroperabilitas/static/js/runtime-main.79c27285.js"
+  }
+]);
